f52ecbe5471ed458b24633208522ef2775f2825f
nutty7t <blackhole@nutty.email>
2019-03-12 22:41:18 -0700

    Refactor Dockerfile into `_install.sh` scripts
    Modularize the Dockerfile into topical installation scripts and then
    invoke them in the Dockerfile. This will keep the Dockerfile neat and
    tidy.

---
+++ vim/_install.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Create some symbolic links, creating needless indirection, just
+# because I prefer .vimrc over init.vim.
+mkdir --parents ~/.config/nvim \
+	&& ln --force --symbolic ~/Code/dotfiles/vim ~/.vimrc \
+	&& ln --force --symbolic ~/.vimrc/main.vim ~/.config/nvim/init.vim
+
+# Install vim-plug.
+curl \
+	--create-dirs \
+	--fail \
+	--location \
+	--output ~/.local/share/nvim/site/autoload/plug.vim \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Allow support for Python plugins in nvim.
+# Required for deoplete.
+pip3 install --user pynvim
+
+# Install vim plugins. nvim won't exit with a non-zero exit code if
+# +PlugInstall fails so the output of +PlugStatus needs to be parsed in
+# order to determine whether the installation succeeded or not.
+#
+#    Finished. 0 error(s).
+#    [====================]
+#
+#    - foobar: OK
+#    - bazqux: OK
+#        .     .
+#        .     .
+#        .     .
+#
+# In a subshell, exit with the status code as the number of errors that
+# occurred when checking the status of the plugins so that the docker
+# build fails if there are any errors.
+nvim +PlugInstall +qall \
+	&& nvim +PlugStatus +"w PlugStatus" +qall \
+	&& (exit $( \
+		cat PlugStatus \
+		| head --lines=1 \
+		| egrep --only-matching '[0-9+]' \
+	)) \
+	&& rm PlugStatus
+
