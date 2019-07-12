3b53148b931675f84d1252a65531e557e707bc47
nutty7t <blackhole@nutty.email>
2019-07-12 14:24:18 -0700

    Install Bash language server

--- npm/_install.sh
+++ npm/_install.sh
@@ -2,6 +2,17 @@
 
 # TODO: Figure out how to install npm packages globally without sudo.
 
+# NPM runs globally installed module scripts as the `nobody` user,
+# but NPM also sets file permissions for node_modules for the `root`
+# user. This means that scripts running as `nobody` can't write files.
+# This is a workaround:
+#
+# https://stackoverflow.com/a/45505787
+sudo npm -g config set user root
+
+# Bash language server.
+sudo npm install -g bash-language-server
+
 # Dockerfile language server.
 sudo npm install -g dockerfile-language-server-nodejs
 
