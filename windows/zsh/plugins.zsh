6630b4d9292c14567fa37e6be6557a1d0999abcc
nutty7t <blackhole@nutty.email>
2019-12-21 12:22:35 -0700

    Add zsh configuration to Windows dotfiles

---
+++ windows/zsh/plugins.zsh
@@ -0,0 +1,12 @@
+# ──────────────────────────────────────────────────────────────────────
+#  Source the plugins.
+# ──────────────────────────────────────────────────────────────────────
+
+plugins="$HOME/.zsh_plugins"
+
+if [[ -d ${plugins} ]]; then
+	source ${plugins}/zsh-autosuggestions/zsh-autosuggestions.zsh
+	source ${plugins}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+	source ${plugins}/oh-my-zsh/plugins/git/git.plugin.zsh
+	source ${plugins}/oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
+fi
---
+++ windows/zsh/prompt.zsh
@@ -0,0 +1,13 @@
+# ──────────────────────────────────────────────────────────────────────
+#  Spaceship Prompt.
+# ──────────────────────────────────────────────────────────────────────
+
+fpath=("${HOME}/.zfunctions" ${fpath})
+autoload -U promptinit; promptinit
+prompt spaceship
+
+SPACESHIP_CHAR_SYMBOL=":: "
+SPACESHIP_PROMPT_ORDER=(
+	dir
+	char
+)
