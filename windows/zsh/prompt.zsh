b710cd0a736b439ce3e977a189e1f19e51c3fd49
ナッティ <hello@nutty.cc>
2019-12-25 00:51:11 -0700

    Convert spaces to tabs

--- windows/zsh/prompt.zsh
+++ windows/zsh/prompt.zsh
@@ -18,53 +18,53 @@ SPACESHIP_PROMPT_ORDER=(
 # ──────────────────────────────────────────────────────────────────────
 
 spaceship_nix() {
-  # Precondition: The `nix` section is enabled.
-  [[ $SPACESHIP_NIX_SHELL_SHOW == false ]] && return
+	# Precondition: The `nix` section is enabled.
+	[[ $SPACESHIP_NIX_SHELL_SHOW == false ]] && return
 
-  # Precondition: We are in a nix-shell.
-  [[ -z $IN_NIX_SHELL ]] && return
+	# Precondition: We are in a nix-shell.
+	[[ -z $IN_NIX_SHELL ]] && return
 
-  echo -n "%{%F{green}%}"
-  if grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
-    # HACK: For whatever reason, at least in the terminal
-    # emulators on Windows, it seems as if zsh uses the
-    # number of bytes in the UTF-8 representation of Unicode
-    # characters to determine their widths. This causes
-    # wonky formatting, so I'm using zsh escape sequences to
-    # manually specify the width of the characters.
-    echo -n "\xCE%{\xBB%} "
-  else
-    echo -n "λ "
-  fi
-  echo -n "%{%f%}"
+	echo -n "%{%F{green}%}"
+	if grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
+		# HACK: For whatever reason, at least in the terminal
+		# emulators on Windows, it seems as if zsh uses the
+		# number of bytes in the UTF-8 representation of Unicode
+		# characters to determine their widths. This causes
+		# wonky formatting, so I'm using zsh escape sequences to
+		# manually specify the width of the characters.
+		echo -n "\xCE%{\xBB%} "
+	else
+		echo -n "λ "
+	fi
+	echo -n "%{%f%}"
 
-  # Invoked using `nix-shell -p [packages]`
-  if [[ -n ${NIX_SHELL_PACKAGES} ]]; then
-    local packageNames=""
-    local packages=(${NIX_SHELL_PACKAGES})
+	# Invoked using `nix-shell -p [packages]`
+	if [[ -n ${NIX_SHELL_PACKAGES} ]]; then
+		local packageNames=""
+		local packages=(${NIX_SHELL_PACKAGES})
 
-    # Output package names. This list can get long.
-    # If it seems too long, the user should consider
-    # putting this inside of a Nix expression.
-    echo -n "[ "
-    for package in ${packages}; do
-      echo -n "${package} "
-    done
-    echo -n "] "
+		# Output package names. This list can get long.
+		# If it seems too long, the user should consider
+		# putting this inside of a Nix expression.
+		echo -n "[ "
+		for package in ${packages}; do
+			echo -n "${package} "
+		done
+		echo -n "] "
 
-  # Otherwise, invoked using nix expression file.
-  else
-    local cleanName=${name#interactive-}
-    cleanName=${cleanName%-environment}
-    echo -n "${cleanName} "
-  fi
+	# Otherwise, invoked using nix expression file.
+	else
+		local cleanName=${name#interactive-}
+		cleanName=${cleanName%-environment}
+		echo -n "${cleanName} "
+	fi
 
-  # Print suffix.
-  echo -n "%{%F{green}%}"
-  if grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
-    echo -n "\xE2%{\x86\x92%} "
-  else
-    echo -n "→ "
-  fi
-  echo -n "%{%f%}"
+	# Print suffix.
+	echo -n "%{%F{green}%}"
+	if grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
+		echo -n "\xE2%{\x86\x92%} "
+	else
+		echo -n "→ "
+	fi
+	echo -n "%{%f%}"
 }
