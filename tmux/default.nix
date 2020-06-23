c51a23a0465ffb4bed071921073199f496b90943
nutty7t <blackhole@nutty.email>
2020-06-22 17:35:45 -0700

    Fix `vim-tmux-navigator`
    I couldn't figure out how to get plugins working inside of the nix
    derivation so I just inlined the configuration instead.

--- tmux/default.nix
+++ tmux/default.nix
@@ -1,17 +1,11 @@
 { makeWrapper, symlinkJoin, tmux, tmuxPlugins }:
-  let
-    plugins = with tmuxPlugins;
-      [
-        vim-tmux-navigator
-      ];
-  in
-    symlinkJoin {
-      name = "tmux";
-      buildInputs = [ makeWrapper ];
-      paths = [ tmux ];
-      postBuild = ''
-        wrapProgram "$out/bin/tmux" \
-          --add-flags "-f ${./tmux.conf}" \
-          --add-flags "-u"
-      '';
-    }
+  symlinkJoin {
+    name = "tmux";
+    buildInputs = [ makeWrapper ];
+    paths = [ tmux ];
+    postBuild = ''
+      wrapProgram "$out/bin/tmux" \
+        --add-flags "-f ${./tmux.conf}" \
+        --add-flags "-u"
+    '';
+  }
