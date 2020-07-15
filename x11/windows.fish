f132daf2ff99d60308a0a653cb7157cd3baaa2b3
nutty7t <blackhole@nutty.email>
2020-07-14 20:58:31 -0700

    Suppress conditional check output

--- x11/windows.fish
+++ x11/windows.fish
@@ -1,5 +1,5 @@
 if grep --ignore-case "microsoft" /proc/version &> /dev/null
-    if not type -f /.dockerenv
+    if not type -f /.dockerenv > /dev/null 2>&1
         set --export DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'):0.0
         set --export LANG C
     end
