333c67e6cab7bfcfb4d0563aa3d3ac01cc0d09cb
nutty7t <blackhole@nutty.email>
2020-08-04 19:35:41 -0700

    Launch the GPG agent

--- gpg/variables.fish
+++ gpg/variables.fish
@@ -6,3 +6,4 @@ set --export GPG_TTY (tty)
 set keygrip BDA0F0D14EC318874C7872020155BE3371806A42
 cat ~/.gnupg/sshcontrol | grep $keygrip > /dev/null; or echo $keygrip >> ~/.gnupg/sshcontrol
 pgrep ssh-agent > /dev/null; or eval (ssh-agent -c)
+gpgconf --launch gpg-agent
