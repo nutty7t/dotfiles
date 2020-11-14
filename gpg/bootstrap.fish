023e10bcb866a3d42cff94e3641109ccb311ad13
nutty7t <blackhole@nutty.email>
2020-11-13 18:14:34 -0700

    Remove unnecessary `ssh-agent` launch

--- gpg/bootstrap.fish
+++ gpg/bootstrap.fish
@@ -6,8 +6,8 @@ set --export GPG_TTY (tty)
 set keygrip 2488D6BC80912B868503FADA03341331CAE89FA7
 cat ~/.gnupg/sshcontrol | grep $keygrip >/dev/null; or echo $keygrip >>~/.gnupg/sshcontrol
 
-pgrep ssh-agent >/dev/null; or eval (ssh-agent -c)
-gpgconf --launch gpg-agent >/dev/null
+# Launch gpg-agent if it's not already running.
+pgrep gpg-agent >/dev/null; or gpgconf --launch gpg-agent >/dev/null
 
 # Change the standard display so that pinentry works. Not sure why this works.
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=835394
