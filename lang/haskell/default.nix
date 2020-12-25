3c3b9b86faa820fe6c49f0604956908a93833853
nutty7t <blackhole@nutty.email>
2020-12-24 19:09:55 -0700

    Install more haskell packages

--- lang/haskell/default.nix
+++ lang/haskell/default.nix
@@ -1,9 +1,12 @@
 { config, pkgs, ... }:
   {
     home.packages = [
+      pkgs.haskellPackages.cabal-install
+      pkgs.haskellPackages.ghc
       pkgs.haskellPackages.hlint
       pkgs.haskellPackages.hoogle
       pkgs.haskellPackages.ormolu
+      pkgs.haskellPackages.stack
     ];
 
     home.file = {
